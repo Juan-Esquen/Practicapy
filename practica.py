c627564b4f0efcf92cde97c51b06b726cc85dd58
Juan Esquen <esquenjuan11@otlook.com>
2024-03-02 18:12:44 -0500

    Commit inicial

---
+++ practica.py
@@ -0,0 +1,11 @@
+number1 = 1
+number2 = 3
+Lista = [1, 3, 5, 31, 7, 23]
+
+sum = number1 + number2
+Nombre = input("Escriba su nombre ")
+
+print("Hola", Nombre, "Mira estos dos números", number1, number2)
+print("¿Cuánto es la suma?")
+input()
+print("La respuesta es", sum)
